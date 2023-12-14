68a2bc4f59918d1a4a6f9fc6ba688c1b04b28721
KasperPtak <kasperptak2@gmail.com>
2023-12-14 17:04:49 +0100

    Spacing, nothing new added

--- models/post.js
+++ models/post.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
     /**
@@ -10,21 +8,20 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      
       Post.belongsTo(models.Status, {
-        foreignKey: 'status_id'
-      })
-      
+        foreignKey: "status_id",
+      });
+
       Post.belongsTo(models.Category, {
-        foreignKey: 'category_id'
-      })
+        foreignKey: "category_id",
+      });
 
       Post.belongsTo(models.User, {
-        foreignKey: 'user_id'
-      })
+        foreignKey: "user_id",
+      });
 
-      Post.hasMany(models.Comment, { 
-        foreignKey: 'post_id' 
+      Post.hasMany(models.Comment, {
+        foreignKey: "post_id",
       });
       // Post.belongsToMany(models.User, {
       //   through: 'PostHasUpvote',
@@ -33,19 +30,22 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Post.init({
-    title: DataTypes.STRING,
-    upvotes: DataTypes.INTEGER,
-    content: DataTypes.STRING,
-    comment_id: DataTypes.INTEGER,
-    category_id: DataTypes.INTEGER,
-    user_id: DataTypes.INTEGER,
-    upvotes_id: DataTypes.INTEGER,
-    status_id: DataTypes.INTEGER,
-    image: DataTypes.STRING,
-  }, {
-    sequelize,
-    modelName: 'Post',
-  });
+  Post.init(
+    {
+      title: DataTypes.STRING,
+      upvotes: DataTypes.INTEGER,
+      content: DataTypes.STRING,
+      comment_id: DataTypes.INTEGER,
+      category_id: DataTypes.INTEGER,
+      user_id: DataTypes.INTEGER,
+      upvotes_id: DataTypes.INTEGER,
+      status_id: DataTypes.INTEGER,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Post",
+    }
+  );
   return Post;
 };
