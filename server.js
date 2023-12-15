92d21cb59993f80b0fa9df754338b481084c3d2c
KasperPtak <kasperptak2@gmail.com>
2023-12-15 15:22:03 +0100

    Revert "changed port to work with nginx, and hosting"
    This reverts commit 1fdad8c9f3aa086071b0acfe26d2c4189266511a.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes/routes');
 const cors = require('cors');
 const verifyController = require('./controllers/verifyController.js')
-const port = 80; 
+const port = 1234; 
 const { cronSchedueler } = require('./tasks/scheduler.js')
 
 const app = express();
