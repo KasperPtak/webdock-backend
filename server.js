135315c07c84a725d013672715276a07b59c6b99
KasperPtak <kasperptak2@gmail.com>
2023-12-15 16:07:25 +0100

    Revert back to og
    Co-Authored-By: soklope <95622881+soklope@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,30 +1,25 @@
-const https = require('https');
 const express = require('express');
-const fs = require('fs');
 const routes = require('./routes/routes');
 const cors = require('cors');
-const verifyController = require('./controllers/verifyController.js');
-const { cronSchedueler } = require('./tasks/scheduler.js');
+const verifyController = require('./controllers/verifyController.js')
+const port = 1234; 
+const { cronSchedueler } = require('./tasks/scheduler.js')
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {origin: "*"}
+))
 app.use(express.json());
 
 cronSchedueler();
-
 const mailer = require('express-mailer');
 const mailerConfig = require('./config/emailConfig/emailConfig.js');
+
 mailer.extend(app, mailerConfig);
 
 app.use('/api/v1', routes);
 app.post('/verify', verifyController.verifyUser);
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/kmfpgroup5.vps.webdock.cloud/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/kmfpgroup5.vps.webdock.cloud/fullchain.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(443, () => {
-  console.log('HTTPS Server running on port 443');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
